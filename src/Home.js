61e0f7c1ad83c95dc408e86b639212769b9fe2e6
jperalta <jperalta.henri@gmail.com>
2019-04-09 21:13:14 -0400

    initial setup of header and home components

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from './Header'
+
+class Home extends Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default Home;
